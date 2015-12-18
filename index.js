f11e29abc78154294b75c01135e28ac2aedeea61
Raphael Jarsaillon <raphael.jarsaillon@viseo.com>
2015-12-18 17:15:46 +0100

    merge

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static(__dirname + '/'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
