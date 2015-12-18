858b1278cb5aa649650ddbd36e298d07000e3319
Raphael Jarsaillon <raphael.jarsaillon@viseo.com>
2015-12-18 17:50:55 +0100

    Fusion polymer et socket.io

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat_msg', function(msg){
+  	console.log('message received');
+    io.emit('chat_msg', msg);
   });
 });
 
