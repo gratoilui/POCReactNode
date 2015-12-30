71122cf3318a02b6fce5bb7c02bdc9759c0da936
Jonathan Soler <jonathan.soler@viseo.com>
2015-12-30 16:46:53 +0100

    Ajout de la gestion des utilisateurs. Connection et deconnection, mise à jour de la liste en fonction.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# POCReactNode
+# POCRolymerNode
+
+To launch :
+-npm install
+-bower install
+-node index.js
+=> http://localhost:3000/app/whiteboard.html for whiteboard
+=> http://localhost:3000/app/whiteboardUser.html for users
--- index.js
+++ index.js
@@ -5,17 +5,43 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/'));
 
+var users =[]; // liste des utilisateurs connectés
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat_msg', function(msg){
-  	console.log('message received');
-    io.emit('chat_msg', msg);
-  });
+    //event lors de la déconnection d'un client
+    socket.on('disconnect', function(){
+        //On recherche dans la liste des utilisateurs connectés
+        //l'tulisateur qui se déconnecte pour le supprimer
+        for(var i = 0 ; i < users.length; i++) {
+            if(users[i].id == socket.id){
+                console.log('Déconnection de '+users[i].user);
+                var msg = {username : users[i].user};
+                //on informe les écrans "board" de la déconnection de l'utilisateur pour qu'ils mettent
+                //leur liste d'utilisateur à jour
+                io.emit('removeuser', msg);
+                break;
+            }
+        }
+        console.log('user disconnected');
+    });
+    //event d'arrivé d'un message provenant des clients
+    socket.on('chat_msg', function(msg){
+        console.log('message received');
+        io.emit('chat_msg', msg);
+    });
+    //event d'enregistrment d'un utilisateur
+    socket.on('chat_cnx', function(msg){
+        console.log('connection de l\'utilisateur ' +msg.username);
+        if(msg.username != '') {
+            console.log(socket.id);
+            users.push({id : socket.id, user : msg.username});
+            //on informe les écrans "board" de la connection de l'utilisateur avec son nom, pour qu'ils mettent
+            //leur liste d'utilisateur à jour
+            io.emit('adduser', msg);
+        }
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
