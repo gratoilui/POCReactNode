a479d116f44316b894150178bce74d2003f0688d
Jonathan Soler <jonathan.soler@viseo.com>
2016-01-24 16:59:10 +0100

    Sauvegarde des textes ajoutés ainsi que des utilisateurs pour que lors de la connexion d'un nouvel utilisateur, il soit au même niveau que ceux déjà connectés

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/'));
 
 var users = {}; // liste des utilisateurs connectés
+var textSave = []; //Ensemble des textes écrit avec leur position et leur couleur pour les nouveaux arrivant
 
 //Fonction pour générer une couleur aléatoire
 function randomColor() {
@@ -21,15 +22,15 @@ io.on('connection', function(socket){
     //event lors de la déconnection d'un client
     socket.on('disconnect', function(){
         //On recherche dans la liste des utilisateurs connectés
-        //l'tulisateur qui se déconnecte pour le supprimer
+        //l'utilisateur qui se déconnecte pour le supprimer
         var user = users[socket.id];
         if(user) {
+            delete users[socket.id]; // on supprime l'objet de la liste
             var msg = {username: user.name};
             //on informe les écrans "board" de la déconnection de l'utilisateur pour qu'ils mettent
             //leur liste d'utilisateur à jour
             io.emit('removeuser', msg);
         }
-
         console.log('user disconnected');
     });
     //event d'arrivé d'un message provenant des clients
@@ -37,6 +38,7 @@ io.on('connection', function(socket){
         console.log('message received');
         var user = users[socket.id];
         msg.color = user.color;
+        textSave.push(msg); //on garde en memoire le nouveau message pour pouvoir les envoyer aux nouveaux connectés
         io.emit('chat_msg', msg);
     });
     //event d'enregistrment d'un utilisateur
@@ -55,6 +57,16 @@ io.on('connection', function(socket){
             //leur liste d'utilisateur à jour
             io.emit('adduser', msg);
         }
+    });
+    //event d'initialisation du canvas avec les messages existants
+    socket.on('init', function(msg){
+        //on envoie au nouveau connecté les précédents textes
+        io.emit('init'+msg.counter, {textSave : textSave});
+    });
+    //event d'initialisation de la listes des utilisateurs
+    socket.on('initUsers', function(msg){
+        //on envoie au nouveau connecté les précédents textes
+        io.emit('initUsers'+msg.counter, {users : users});
     });
 });
 
